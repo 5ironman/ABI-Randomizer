5dd798d7c272e57f3c35bcc10a4ceb51e23fb26b
5ironman <jmlong902@gmail.com>
2026-02-22 20:50:10 -0500

    Update Randomizer.py

--- Randomizer.py
+++ Randomizer.py
@@ -126,59 +126,69 @@ WEAPONS_DATA = {
 # OTHER DATA
 # ----------------------
 ammo_data = {
-    "5.45x39mm": ["HP", "PS", "BP", "BS", "PP", "PRS"],
-    "5.56x45mm": ["HP Hunting", "FMJ Hunting", "M193", "M855", "M855A1", "M995"],
-    "5.8x42mm": ["DBP87", "DVP88", "DVC12"],
-    "7.62x39mm": ["HP", "LP", "US", "T45M", "PS", "BP", "AP"],
-    "7.62x51mm": ["UN", "BPZ", "M80", "M62", "M61"],
-    "7.62x54mm": ["LPS", "T46M", "7BT1", "SNB", "7N37"],
-    ".338 Lapua": ["UPZ", "FMJ", "AP"],
-    ".44 Cal": ["LFNP", "SJHP", "JSP"],
-    ".45 ACP": ["HS", "FMJ", "AP"],
-    "7.62x25mm": ["PT", "PST", "LRN", "AKBS", "PS"],
-    "9x19mm": ["PSO", "PST", "AP6.3", "DumDum", "7N31"],
-    "12x70mm": ["Type 5 buckshot", "Type 7 buckshot", "Type 8 buckshot", "Flechette Buckshot", "Dual Shell", "Led Slug", "Grizzly Slug", "RIP Slug", "GT Slug", "AP Slug"],
-    "5.7x28mm": ["SS197SR", "SS190", "R37.X", "L191", "SS198"],
-    "9x39mm": ["SP5", "SP6", "7N9", "7N12"]
-}
+        "5.45x39mm": ["HP", "PS", "BP", "BS", "PP", "PRS"],
+        "5.56x45mm": ["HP Hunting", "FMJ Hunting", "M193", "M855", "M855A1", "M995"],
+        "5.8x42mm": ["DBP87", "DVP88", "DVC12"],
+        "7.62x39mm": ["HP", "LP", "US", "T45M", "PS", "BP", "AP"],
+        "7.62x51mm": ["UN", "BPZ", "M80", "M62", "M61"],
+        "7.62x54mm": ["LPS", "T46M", "7BT1", "SNB", "7N37"],
+        ".338 Lapua": ["UPZ", "FMJ", "AP"],
+        ".44 Cal": ["LFNP", "SJHP", "JSP"],
+        ".45 ACP": ["HS", "FMJ", "AP"],
+        "7.62x25mm": ["PT", "PST", "LRN", "AKBS", "PS"],
+        "9x19mm": ["PSO", "PST", "AP6.3", "DumDum", "7N31"],
+        "12x70mm": ["Type 5 buckshot", "Type 7 buckshot", "Type 8 buckshot", "Flechette Buckshot", "Dual Shell", "Led Slug", "Grizzly Slug", "RIP Slug", "GT Slug", "AP Slug"],
+        "5.7x28mm": ["SS197SR", "SS190", "R37.X", "L191", "SS198"],
+        "9x39mm": ["SP5", "SP6", "7N9", "7N12"]
+    }
 
-armors = {
-    "Tier 1": ["Retro Sapper Bulletproof Vest", "Retro Bulletproof Vest", "Old Security Body Armor"],
-    "Tier 2": ["Security Body Armor", "220 Body Armor", "Retro Infantry Bulletproof Vest"],
-    "Tier 3": ["KN Regulation Body Armor", "PCA350 Body Armor", "Standard SWAT Armor",
-               "H-Tac SWAT Body Armor", "KN Assault Body Armor", "H-LC Lightweight Body Armor"],
-    "Tier 4": ["SEK Fortress Body Armor", "IND401 Body Armor", "6B13 Body Armor",
-               "6B23 Body Armor", "Spartan B Body Armor"],
-    "Tier 5": ["H-LC Tactical Body Armor", "Defender M4 Heavy Body Armor (Black)",
-               "Defender M4 Heavy Body Armor (Green)", "926 Composite Body Armor",
-               "IMTV Samurai Assault Armor", "IMTV Samurai Standard Armor", "IMTV Samurai Full Protection",
-               "BT6 Heavy Body Armor"],
-    "Tier 6": ["Marshal Heavy Body Armor", "6B45 Heavy Body Armor", "BT101 Tactical Body Armor",
-               "KN Composite Body Armor"]
-}
+    armors = {
+        "Tier 1": ["Retro Sapper Bulletproof Vest", "Retro Bulletproof Vest", "Old Security Body Armor"],
+        "Tier 2": ["Security Body Armor", "220 Body Armor", "Retro Infantry Bulletproof Vest"],
+        "Tier 3": ["KN Regulation Body Armor", "PCA350 Body Armor", "Standard SWAT Armor",
+                   "H-Tac SWAT Body Armor", "KN Assault Body Armor", "H-LC Lightweight Body Armor"],
+        "Tier 4": ["SEK Fortress Body Armor", "IND401 Body Armor", "6B13 Body Armor",
+                   "6B23 Body Armor", "Spartan B Body Armor"],
+        "Tier 5": ["H-LC Tactical Body Armor", "Defender M4 Heavy Body Armor (Black)",
+                   "Defender M4 Heavy Body Armor (Green)", "926 Composite Body Armor",
+                   "IMTV Samurai Assault Armor", "IMTV Samurai Standard Armor", "IMTV Samurai Full Protection",
+                   "BT6 Heavy Body Armor"],
+        "Tier 6": ["Marshal Heavy Body Armor", "6B45 Heavy Body Armor", "BT101 Tactical Body Armor",
+                   "KN Composite Body Armor"]
+    }
 
-helmets = {
-    "Tier 1": ["Kelsey Fire Helmet", "Lightweight Safety Helmet", "Motorcycle Helmet", "Tanker Protective Cap"],
-    "Tier 2": ["Retro Military Helmet", "Retro Steel Helmet", "Security Helmet", "Aviator Helmet",
-               "Security Riot Helmet", "PAS Standard Helmet"],
-    "Tier 3": ["PAS2 Helmet", "F70 Tactical Helmet", "SH12 Military Helmet", "6B4 Helmet",
-               "6B4 Helmet (Squad S)", "6B5 Helmet"],
-    "Tier 4": ["SH40 Military Helmet", "IND Tactical Helmet", "IND Tactical Helmet (Variant)", "IND200 Helmet",
-               "F80 Tactical Helmet", "SH18 Military Helmet", "KSS Tactical Helmet", "KSS2 Tactical Helmet",
-               "56K Helicopter Helmet"],
-    "Tier 5": ["SH Matzka 2 Helmet", "SH60 Military Helmet", "SH50 Military Helmet", "FA Assault Tactical Helmet",
-               "03 Heavy Tactical Helmet", "RSP Heavy Tactical Helmet", "AN95 Heavy Blast Helmet"],
-    "Tier 6": ["6BNT Helmet", "RST Special Forces Helmet", "HGB4 Offensive Helmet", "SH65 Military Helmet",
-               "IND50 Heavy Tactical Helmet", "D009 Blast Helmet", "AS200 Heavy Tactical Helmet"]
-}
+    armored_rigs = {
+        "Tier 2": ["M1955 Combat Vest"],
+        "Tier 3": ["6B5 Armored Rig", "Sentry 3 Armored Chest", "926 Security Armored Rig"],
+        "Tier 4": ["Sentry 305 Armored Rig", "TM1 Armored Rig", "TM2 Armored Rig"],
+        "Tier 5": ["H-Tac A8 Armored Rig", "Warrior Heavy Armored Rig", "H-Tac A9 Armored Rig",
+                   "Defender M4 Heavy Armored Rig"],
+        "Tier 6": ["Spartan C Heavy Armored Rig", "AL Tactical Armored Rig", "AVS Heavy Armored Rig",
+                   "AL Commander Armored Rig", "AL Assault Armored Rig"]
+    }
 
-Backpacks = [
-    "Sling Bag","Lightweight Camping Backpack","Medium Camping Backpack","Simple Backpack","Canvas Backpack",
-    "Canvas Camping Backpack","Sports Backpack","Cowhide Backpack","Outdoor Travel Backpack",
-    "RUSH Tactical Backpack","Large Camping Backpack","XA4 Tactical Backpack","Med Field Backpack",
-    "Chapman Military Backpack","AMP7 Assault Backpack","Retro Marching Backpack","LUC Expanded Tactical Backpack",
-    "926 Field Backpack","Field Camping Backpack","RAL Heavy Military Backpack"
-]
+    helmets = {
+        "Tier 1": ["Kelsey Fire Helmet", "Lightweight Safety Helmet", "Motorcycle Helmet", "Tanker Protective Cap"],
+        "Tier 2": ["Retro Military Helmet", "Retro Steel Helmet", "Security Helmet", "Aviator Helmet",
+                   "Security Riot Helmet", "PAS Standard Helmet"],
+        "Tier 3": ["PAS2 Helmet", "F70 Tactical Helmet", "SH12 Military Helmet", "6B4 Helmet",
+                   "6B4 Helmet (Squad S)", "6B5 Helmet"],
+        "Tier 4": ["SH40 Military Helmet", "IND Tactical Helmet", "IND Tactical Helmet (Variant)", "IND200 Helmet",
+                   "F80 Tactical Helmet", "SH18 Military Helmet", "KSS Tactical Helmet", "KSS2 Tactical Helmet",
+                   "56K Helicopter Helmet"],
+        "Tier 5": ["SH Matzka 2 Helmet", "SH60 Military Helmet", "SH50 Military Helmet", "FA Assault Tactical Helmet",
+                   "03 Heavy Tactical Helmet", "RSP Heavy Tactical Helmet", "AN95 Heavy Blast Helmet"],
+        "Tier 6": ["6BNT Helmet", "RST Special Forces Helmet", "HGB4 Offensive Helmet", "SH65 Military Helmet",
+                   "IND50 Heavy Tactical Helmet", "D009 Blast Helmet", "AS200 Heavy Tactical Helmet"]
+    }
+
+    Backpacks = [
+        "Sling Bag", "Lightweight Camping Backpack", "Medium Camping Backpack", "Simple Backpack", "Canvas Backpack",
+        "Canvas Camping Backpack", "Sports Backpack", "Cowhide Backpack", "Outdoor Travel Backpack",
+        "RUSH Tactical Backpack", "Large Camping Backpack", "XA4 Tactical Backpack", "Med Field Backpack",
+        "Chapman Military Backpack", "AMP7 Assault Backpack", "Retro Marching Backpack", "LUC Expanded Tactical Backpack",
+        "926 Field Backpack", "Field Camping Backpack", "RAL Heavy Military Backpack"
+    ]
 
 # ----------------------
 # ENSURE BUILD CODES
@@ -304,5 +314,6 @@ for weapon, codes in st.session_state.build_codes.items():
 
 # Display all codes for reference
 st.json(st.session_state.build_codes)
+
 
 
